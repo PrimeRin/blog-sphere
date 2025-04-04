af59f2fe6691333a79c44ae04f2d337e1308749c
PrimeRin <primedruk312@gmail.com>
2025-04-04 15:36:32 +0600

    topics ui done

--- src/templates/topics.php
+++ src/templates/topics.php
@@ -1,3 +1,33 @@
-<div>
-    Topics
-</div>
+<div class="topics-container">
+        <?php
+        $names = [
+            "Sonam",
+            "Sonam Kapoor",
+            "Sonam Bajwa",
+            "Sonam Wangchuk",
+            "Sonamarg",
+            "Sonambulismo",
+            "Sonamhotphotoshoot",
+            "Sonamhotphotos",
+            "Sonambralessphotosshoot",
+            "Sonam Makeup Studio",
+            "Sonam Kapoor Hot Images",
+            "Sonam Kapoor Bikni Photos",
+            "Sonam Kapoor Hot Photos",
+            "Sonam Kapoor Bikni Images",
+            "Sonam Gupta",
+            "Sonam Bajwa Biography",
+            "Sonamwangchuck",
+            "Sonamasoori",
+            "Sonámbula",
+            "Sonamlotus",
+            "Sonamiller",
+            "Sonambulo",
+            "Sonambient"
+        ];
+
+        foreach ($names as $name) {
+            echo '<div class="topics-tag">' . htmlspecialchars($name) . '</div>';
+        }
+        ?>
+    </div>
